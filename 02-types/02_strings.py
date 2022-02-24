19c2ed1d0fa4282580cd67f2aa0f90ddbba3d61c
ritaly <malgorzata.lyczywek@gmail.com>
2022-02-24 21:53:32 +0100

    exercies strings

---
+++ 02-types/02_strings.py
@@ -0,0 +1,6 @@
+# s1 = 'lala'
+# s2 = 'tata'
+#
+# result -> 'latatala'
+#
+# z użyciem len()
---
+++ 02-types/03_strings.py
@@ -0,0 +1,19 @@
+quote = "Honesty is the first chapter in the book of wisdom."
+
+# Policz wszystkie znaki w napisie
+#
+# Nie modyfikując zmiennej wyświetl słowo wisdom
+
+print(quote[-7:-1])
+
+# Wyświetl tylko pierwszą połowę tekstu
+#
+# Wyświetl tylko kropkę
+#
+# Wyświetl od połowy tylko co trzecią literę cytatu
+#
+# Wyświetl ‘Hnsyi h is hpe ntebo fwso.’
+#
+# Wyświetl cały cytat odwrotnie
+#
+# Zamień wisdom na słowo friendship
